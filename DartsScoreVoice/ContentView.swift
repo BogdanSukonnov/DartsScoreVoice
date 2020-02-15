09913bbab6e7d033ea90645c6e9ff539b2e85843
Bogdan Sukonnov <bogdansukonnov@gmail.com>
2020-02-15 09:38:02 +0300

    Initial Commit

---
+++ DartsScoreVoice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DartsScoreVoice
+//
+//  Created by Bogdan Sukonnov on 2/15/20.
+//  Copyright © 2020 Evermind. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
